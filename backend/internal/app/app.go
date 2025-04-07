431359869264a2af603f6a44f921b563102f84eb
topotijka <potap2199@gmail.com>
2025-04-08 00:02:13 +0300

    some fixes

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -2,18 +2,24 @@ package app
 
 import (
 	"backend/internal/db"
+	task_handler "backend/internal/domain/task/handler"
 	task_repo "backend/internal/domain/task/repository"
 	task_usecase "backend/internal/domain/task/usecase"
 	"backend/internal/server"
+	"net/http"
 )
 
 func Start() {
 	connStr := "TODO"
 	dbConnPool := db.NewDBConnect(connStr)
 
+	r := server.NewRouter()
+
 	taskRepo := task_repo.NewTaskRepo(dbConnPool)
 	taskUsecase := task_usecase.NewTaskUsecase(taskRepo)
 	taskHandler := task_handler.NewTaskHandler(taskUsecase)
 
-	r := server.NewRouter()
+	taskHandler.Register(r)
+
+	http.ListenAndServe("7878", r)
 }
